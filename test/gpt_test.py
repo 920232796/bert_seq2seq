bd52adcbb852014f3571fffc1e15a0bd82d8e321
xingzhaohu <920232796@qq.com>
2021-03-11 17:05:02 +0800

    gpt2-test

--- test/gpt_test.py
+++ test/gpt_test.py
@@ -6,11 +6,14 @@ from bert_seq2seq.utils import load_gpt
 from bert_seq2seq.tokenizer import load_chinese_base_vocab
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+vocab_path = "./state_dict/gpt_vocab.txt"
+model_path = "./state_dict/gpt_pytorch_model.bin"
 if __name__ == "__main__":
-    word2ix = load_chinese_base_vocab("./state_dict/gpt_vocab.txt")
+    word2ix = load_chinese_base_vocab(vocab_path)
     model = load_gpt(word2ix)
     model.eval()
     model.set_device(device)
-    model.load_pretrain_params("./state_dict/gpt_pytorch_model.bin")
+    model.load_pretrain_params(model_path)
 
     print(model.sample_generate("今天天气好", out_max_length=300))
