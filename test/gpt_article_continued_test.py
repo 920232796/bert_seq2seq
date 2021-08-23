719b1b9940d0fda044bcac302448760e9ebd4b58
zhaohu xing <920232796@qq.com>
2021-08-23 15:23:41 +0800

    add readme.md

--- test/gpt_article_continued_test.py
+++ test/gpt_article_continued_test.py
@@ -5,8 +5,8 @@ from bert_seq2seq import load_chinese_base_vocab
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-vocab_path = "./state_dict/gpt_vocab.txt"
-model_path = "./state_dict/gpt_pytorch_model.bin"
+vocab_path = "./state_dict/gpt2通用中文模型/vocab.txt"
+model_path = "./state_dict/gpt2通用中文模型/pytorch_model.bin"
 
 if __name__ == "__main__":
     word2ix = load_chinese_base_vocab(vocab_path)
