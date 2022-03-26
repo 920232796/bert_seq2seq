c2d24f95c19ede78278eb2eda48b44eaa1da21c7
zhaohu xing <920232796@qq.com>
2022-03-26 19:29:14 +0800

    delete some nouse code

--- examples/bart_auto_title_train.py
+++ examples/bart_auto_title_train.py
@@ -3,10 +3,6 @@ import torch
 import time
 import glob
 from torch.utils.data import Dataset, DataLoader
-from bert_seq2seq import T5PegasusTokenizer, load_chinese_base_vocab
-from bert_seq2seq import T5Model
-from bert_seq2seq.bart_chinese import BartGenerationModel
-from bert_seq2seq import Tokenizer
 from tqdm import tqdm
 from bert_seq2seq.extend_model_method import ExtendModel
 
